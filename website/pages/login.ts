d1e8511921c181eaf836f9905c382b7b12978323
MikaPolishook <s333564300@ariel.ort.org.il>
2024-12-12 20:24:59 +0200

    start- login page

---
+++ website/pages/login.ts
@@ -0,0 +1,6 @@
+import { send } from "../utilities"
+
+let UsernameInput = document.querySelector("UsernameInput") as HTMLInputElement;
+let PasswordInput = document.querySelector("PasswordInput") as HTMLInputElement;
+let Loginbutton = document.querySelector("LoginButton") as HTMLButtonElement;
+
--- website/pages/signup.ts
+++ website/pages/signup.ts
@@ -4,6 +4,15 @@ let UsernameInput = document.querySelector("UsernameInput") as HTMLInputElement;
 let PasswordInput = document.querySelector("PasswordInput") as HTMLInputElement;
 let SignUpbutton = document.querySelector("SignUpButton") as HTMLButtonElement;
 
-SignUpbutton.onclick = function() {
-send("signup", [UsernameInput.value, PasswordInput.value]);
+SignUpbutton.onclick = async function() {
+let userId = await send("signup", [UsernameInput.value, PasswordInput.value]);
+}
+
+if (userId != null) {
+    localStorage.setItem("userId", userId);
+    top.location.href = "index.html";
+}
+
+else {
+    messageDiv.innerText = "Username is already taken";
 }
